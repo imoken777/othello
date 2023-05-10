2581ea3905898ef967f40adda7ca87a8b1fd1ebe
imoken777 <imoken777@users.noreply.github.com>
2023-05-10 16:23:01 +0000

    deploy: b4eb2ad5a53a956c0ec7c5f6f046050a5431f8fb

--- _next/static/Q7U3mECsYpu8fdsoVIGz9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ab815382624df5b9.css","static/chunks/pages/index-c6b51dc7c2e2fb58.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/ndPHPVcMxBWugDuJ1eubs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ab815382624df5b9.css","static/chunks/pages/index-b955b35238f54677.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
